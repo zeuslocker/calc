f552ece6e488afc62ddcfb2a2f26263dee25d694
zeuslocker <lapan40@ukr.net>
2016-11-03 17:17:05 +0200

    polish notation

--- calc.rb
+++ calc.rb
@@ -1,136 +1,77 @@
 def calculate(str)
-  ints = '0123456789'
-  fActs = '/*'
-  pm = '+-'
   gstr = str.delete(' ')
+  stack = []
+  out = []
+  ints = '0123456789'
+  var = ''
+  opers = {"(" => 0, ")" => 1, "+" => 3, "-" => 2, "*" => 4, "/" => 5, "^" => 6 }
 
-  while gstr.index('(') do
-gstr[gstr.index('(')..gstr.index(')')] = calculate(gstr[gstr.index('(')+1..gstr.index(')')-1]).to_s
-  end
-gstr.each_char do |sym|
-if fActs.include? sym
-  if sym == '*'
-    ind = gstr.index('*')
-    left = ''
-    right = ''
-    while ints.include?(gstr[ind-=1]) && ind>=0 do
-      left.insert(0, gstr[ind])
-    end
-    if pm.include?(gstr[ind]) && !(left.equal?('')) && ind>=0
-      left.insert(0, gstr[ind])
-      leftTr = ind
-    else
-      leftTr = ind+1
-    end
-    ind = gstr.index('*')
-    while ind< (gstr.length-1) && ints.include?(gstr[ind+=1]) do
-      right.insert(right.length, gstr[ind])
-    end
-    if pm.include?(gstr[ind]) && !(right.equal?(''))
-      right.insert(0, gstr[ind])
-    end
-    if ind < gstr.length-1
-      rightTr = ind-1
-    else
-      rightTr = ind
-    end
-    gstr[leftTr..rightTr] = (left.to_i * right.to_i).to_s
-  else
-    ind = gstr.index('/')
-    left = ''
-    right = ''
-    while ints.include?(gstr[ind-=1]) && ind>=0 do
-      left.insert(0, gstr[ind])
-    end
-    if pm.include?(gstr[ind]) && !(left.equal?('')) && ind>=0
-      left.insert(0, gstr[ind])
-      leftTr = ind
-    else
-      leftTr = ind+1
-    end
-    ind = gstr.index('/')
-    while ind< (gstr.length-1) && ints.include?(gstr[ind+=1]) do
-      right.insert(right.length, gstr[ind])
-    end
-    if pm.include?(gstr[ind]) && !(right.equal?(''))
-      right.insert(0, gstr[ind])
-    end
-    if ind < gstr.length-1
-      rightTr = ind-1
-    else
-      rightTr = ind
-    end
-    gstr[leftTr..rightTr] = (left.to_i / right.to_i).to_s
-  end
-end
-end
+ # transform
   gstr.each_char do |sym|
-    if gstr.index(sym) == 0
+    if ints.include? sym
+      var.insert var.length, sym
       next
+    end
+    if var != ''
+    out<<var
+      var = ''
+    end
+    if sym == opers.key(0)
+      stack << sym
+      next
+    end
+
+    if sym == opers.key(1)
+      while stack.last != opers.key(0)
+        out << stack.pop
+      end
+      stack.pop
+      next
+    end
+
+if opers.has_key? sym
+while opers[sym].to_i < opers[stack.last].to_i
+out << stack.pop
 end
-    if pm.include? sym
-      if sym == '+'
-        ind = gstr.index('+')
-        left = ''
-        right = ''
+  stack << sym
+  next
+end
 
-
-        while ints.include?(gstr[ind-=1]) && ind>=0 do
-          left.insert(0, gstr[ind])
-        end
-        if pm.include?(gstr[ind]) && !(left.equal?('')) && ind>=0
-          left.insert(0, gstr[ind])
-          leftTr = ind
-        else
-          leftTr = ind+1
-        end
-        ind = gstr.index('+')
-        while ind< (gstr.length-1) && ints.include?(gstr[ind+=1]) do
-          right.insert(right.length, gstr[ind])
-        end
-        if pm.include?(gstr[ind]) && !(right.equal?(''))
-          right.insert(0, gstr[ind])
-        end
-        if ind < gstr.length-1
-          rightTr = ind-1
-        else
-          rightTr = ind
-        end
-        gstr[leftTr..rightTr] = (left.to_i + right.to_i).to_s
-      else
-        ind = gstr.index('-')
-        left = ''
-        right = ''
-
-        while ints.include?(gstr[ind-=1]) && ind>=0 do
-
-          left.insert(0, gstr[ind])
-        end
-
-        if pm.include?(gstr[ind]) && !(left == '') && ind>=0
-          left.insert(0, gstr[ind])
-          leftTr = ind
-        else
-          leftTr = ind+1
-        end
-
-        ind = gstr.index('-')
-        while ind< (gstr.length-1) && ints.include?(gstr[ind+=1]) do
-          right.insert(right.length, gstr[ind])
-        end
-        if pm.include?(gstr[ind]) && !(right.equal?(''))
-          right.insert(0, gstr[ind])
-        end
-        if ind < gstr.length-1
-          rightTr = ind-1
-        else
-          rightTr = ind
-        end
-
-        gstr[leftTr..rightTr] = (left.to_i - right.to_i).to_s
-      end
-      end
   end
-  return gstr.to_i
-end
+  if var != ''
+    out<<var
+    var = ''
+  end
+  while stack.empty? == false
+  out << stack.pop
+  end
+puts out.to_s
+  #calculate
+#while out.length != 1
+  i = 0
+  while out.size != 1
+
+    if opers.has_key? out[i]
+      if i-2 < 0
+        puts out
+        out[i-1..i] = 0.send(out[i], out[i-1])
+        puts out
+        break
+      end
+      out[i-2..i] = out[i-2].to_f.send(out[i], out[i-1].to_f)
+      i = 0
+    end
+    i+=1
+  end
+  #out.each_with_index do |s, i|
+  #  if opers.has_key? s
+  #    out[i-2..i] = out[i-2].to_f.send(s, out[i-1].to_f)
+  #puts out.to_s
+  #    i = 0
+  #  end
+
+  #end
+  return out[0].to_f
+
+  end
 
